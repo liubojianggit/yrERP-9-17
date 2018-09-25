4ad75940b87eee359e13993e6365512dca25c30a
Test <1194733981@qq.com>
2018-09-25 15:21:21 +0800

    提交部门业务拓展类

--- src/main/java/com/yr/entitys/bo/departmentBo/Departmentbo.java
+++ src/main/java/com/yr/entitys/bo/departmentBo/Departmentbo.java
@@ -1,12 +1,20 @@
 package com.yr.entitys.bo.departmentBo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.yr.entitys.department.Department;
 
-public class Departmentbo {
-    private Department department;
-    private String code;
-    private String name;
-    private String supCode;
+import java.io.Serializable;
+
+//部门业务拓展类
+public class Departmentbo implements Serializable{
+    private Department department;//部门
+    private Integer pid;//父ID
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private String createTime;//创建时间
+    private String createEmp;//创建人
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private String updateTime;//修改时间
+    private String updateEmp;//修改人
 
     public Department getDepartment() {
         return department;
@@ -16,37 +24,54 @@ public class Departmentbo {
         this.department = department;
     }
 
-    public String getCode() {
-        return code;
+    public Integer getPid() {
+        return pid;
     }
 
-    public void setCode(String code) {
-        this.code = code;
+    public void setPid(Integer pid) {
+        this.pid = pid;
     }
 
-    public String getName() {
-        return name;
+    public String getCreateTime() {
+        return createTime;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
     }
 
-    public String getSupCode() {
-        return supCode;
+    public String getCreateEmp() {
+        return createEmp;
     }
 
-    public void setSupCode(String supCode) {
-        this.supCode = supCode;
+    public void setCreateEmp(String createEmp) {
+        this.createEmp = createEmp;
     }
 
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateEmp() {
+        return updateEmp;
+    }
+
+    public void setUpdateEmp(String updateEmp) {
+        this.updateEmp = updateEmp;
+    }
     @Override
     public String toString() {
         return "Departmentbo{" +
                 "department=" + department +
-                ", code='" + code + '\'' +
-                ", name='" + name + '\'' +
-                ", supCode='" + supCode + '\'' +
+                ", pid=" + pid +
+                ", createTime='" + createTime + '\'' +
+                ", createEmp='" + createEmp + '\'' +
+                ", updateTime='" + updateTime + '\'' +
+                ", updateEmp='" + updateEmp + '\'' +
                 '}';
     }
 }
