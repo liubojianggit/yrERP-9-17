c707b17d9cc8d04eb5dfaaff3887353a37f0a130
Test <1194733981@qq.com>
2018-09-25 16:00:28 +0800

    提交部门实体类

--- src/main/java/com/yr/entitys/bo/departmentBo/Departmentbo.java
+++ src/main/java/com/yr/entitys/bo/departmentBo/Departmentbo.java
@@ -9,12 +9,6 @@ import java.io.Serializable;
 public class Departmentbo implements Serializable{
     private Department department;//部门
     private Integer pid;//父ID
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private String createTime;//创建时间
-    private String createEmp;//创建人
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private String updateTime;//修改时间
-    private String updateEmp;//修改人
 
     public Department getDepartment() {
         return department;
@@ -32,46 +26,12 @@ public class Departmentbo implements Serializable{
         this.pid = pid;
     }
 
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getCreateEmp() {
-        return createEmp;
-    }
-
-    public void setCreateEmp(String createEmp) {
-        this.createEmp = createEmp;
-    }
-
-    public String getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(String updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public String getUpdateEmp() {
-        return updateEmp;
-    }
-
-    public void setUpdateEmp(String updateEmp) {
-        this.updateEmp = updateEmp;
-    }
     @Override
-    public String toString() {
+    public String
+    toString() {
         return "Departmentbo{" +
                 "department=" + department +
                 ", pid=" + pid +
-                ", createTime='" + createTime + '\'' +
-                ", createEmp='" + createEmp + '\'' +
-                ", updateTime='" + updateTime + '\'' +
-                ", updateEmp='" + updateEmp + '\'' +
                 '}';
     }
 }
