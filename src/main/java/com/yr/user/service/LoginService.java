d503551f97d89f1b8ba21b981f0e59a94f52c884
Test <1194733981@qq.com>
2018-09-26 21:18:11 +0800

    提交登录模块

--- src/main/java/com/yr/user/service/LoginService.java
+++ src/main/java/com/yr/user/service/LoginService.java
@@ -5,8 +5,16 @@ import com.yr.entitys.user.User;
 import java.util.List;
 
 public interface LoginService {
-    //验证登录用户名是否存在
+
+    /**
+     * 验证登录是否存在
+     * @param loginUser
+     * @return
+     */
     List<User> queryLoginUserName(User loginUser);
-    //验证登录用户名跟密码
+
+    /**
+     *  验证登录用户 账号跟密码是否正确
+     */
     User queryLoginUser(User loginUser);
 }
