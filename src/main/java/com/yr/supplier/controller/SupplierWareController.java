ced6329e4f89758c385466e6031c334d06ec815f
GitNameUser <2922516139@qq.com>
2018-10-02 19:47:59 +0800

    重新更改增删改查

--- src/main/java/com/yr/supplier/controller/SupplierWareController.java
+++ src/main/java/com/yr/supplier/controller/SupplierWareController.java
@@ -4,7 +4,10 @@ import com.yr.core.redis.JedisManager;
 import com.yr.entitys.bo.supplierBO.SupplierBo;
 import com.yr.entitys.bo.supplierBO.SupplierWareBo;
 import com.yr.entitys.page.Page;
+import com.yr.entitys.supplier.SuppWareType;
 import com.yr.entitys.supplier.SupplierWares;
+import com.yr.entitys.user.User;
+import com.yr.supplier.service.SuppWareTypeService;
 import com.yr.supplier.service.SupplierWareService;
 import com.yr.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -29,6 +33,8 @@ import java.util.Map;
 public class SupplierWareController {
     @Autowired
     private SupplierWareService sws;
+    @Autowired
+    private SuppWareTypeService swsType;
     @Autowired
     private JedisManager jedisManager;
     public static String path = "C:/Users/Administrator/Desktop/photo";//图片路径//上传到的地方
@@ -68,11 +74,9 @@ public class SupplierWareController {
     @RequestMapping(value = "/supplierTable/add",method = RequestMethod.GET,produces="application/json;charset=UTF-8")
     public String getAddPage(Map<String , Object>map){
         map.put("supplierWareBo",new SupplierWareBo());
-        Map<String, Object> map1 = new HashMap<>();
-        map1.put("类型1","类型1");
-        map1.put("类型2","类型2");
-        map1.put("类型3","类型3");
-        map.put("typeList",map1);
+        List<SuppWareType> listType=swsType.getSuppWareType();
+        map.put("typeList",listType);
+
         return "supp_waresAU";
     }
 
@@ -84,25 +88,6 @@ public class SupplierWareController {
     @ResponseBody
     @RequestMapping(value = "/supplierTable",method =RequestMethod.POST,produces="application/json;charset=UTF-8")
     public String addWare(SupplierWareBo supplierWareBo, @RequestParam(value="filesCopy",required = false) String filesCopy, HttpServletRequest request){
-       /* String phone = String.valueOf(FileUtils.getTimeStamp());
-        File file = new File(path, phone + ".jpg");//第一个是父级文件路径，第二个是文件名
-        if(!file.getParentFile().exists()){//判断父级路径是否存在
-            file.mkdir();//创建文件夹
-        }
-        if(!file.exists()){//如果文件不存在满足条件
-            try {
-                file.createNewFile();//创建该文件
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        String phoneStr = path + File.separator + phone + ".jpg";//组成一个图片的路径字符串
-        //截取指定路径组成一个本地路径
-        if(!filesCopy.equals("E:\\ideaWork\\yrERP-9-17\\src\\main\\webapp\\images")){//这是默认显示的图片路径
-            filesCopy = request.getServletContext().getRealPath("/") + "photos" + filesCopy.substring(filesCopy.lastIndexOf("\\"),filesCopy.length());
-        }
-        FileUtils.fileCover(phoneStr, filesCopy);//将读取的流覆盖创建的图片
-        supplierWareBo.getSupplierWare().setSuppPhoto(phoneStr);//替换掉原本的路径*/
 
         supplierWareBo.getSupplierWare().setCreateTime(new Date());
         supplierWareBo.getSupplierWare().setCreateEmp("萍");
@@ -132,11 +117,8 @@ public class SupplierWareController {
      */
     @RequestMapping(value = "/supplierTable/{id}",method = RequestMethod.GET,produces="application/json;charset=UTF-8")
     public String upEcho(@PathVariable Integer id,Map<String, Object> map) {
-        Map<String, Object> map1 = new HashMap<>();
-        map1.put("类型1","类型1");
-        map1.put("类型2","类型2");
-        map1.put("类型3","类型3");
-        map.put("typeList",map1);
+        List<SuppWareType> listType=swsType.getSuppWareType();
+        map.put("typeList",listType);
         map.put("supplierWareBo",sws.getById(id));
         return "supp_waresAU";
     }
@@ -151,7 +133,7 @@ public class SupplierWareController {
     public String updateWare(SupplierWareBo supplierWareBo, Map<String,Object>map){
         supplierWareBo.getSupplierWare().setUpdateTime(new Date());
         supplierWareBo.getSupplierWare().setUpdateEmp("萍");
-        //supplierBo.getSupplier().setUpdateEmp(((User)request.getSession().getAttribute("user")).getName());
+       // supplierBo.getSupplier().setUpdateEmp(((User)request.getSession().getAttribute("user")).getName());
         sws.update(supplierWareBo);
         return "{\"code\":1,\"msg\":\"修改成功\"}";
     }
