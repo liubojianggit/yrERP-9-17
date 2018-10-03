5cf5bb9310c764492e1d5876ffc59b3a23da90d9
kunkunxiong <1482569972@qq.com>
2018-10-03 20:06:07 +0800

    权限配置

--- src/main/java/com/yr/user/controller/LoginController.java
+++ src/main/java/com/yr/user/controller/LoginController.java
@@ -8,6 +8,7 @@ import com.yr.user.service.LoginService;
 import com.yr.user.service.UserService;
 import com.yr.util.SerializeUtil;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
